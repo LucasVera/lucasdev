7e87c392363c32f5d38c73b6f72bb838ddf6dc77
LucasVera <lucas@lucasdev.info>
2024-08-26 10:45:43 -0500

    text changes for job description

--- src/pages/Home/Home.texts.ts
+++ src/pages/Home/Home.texts.ts
@@ -1,24 +1,24 @@
 export default {
   en: {
-    jobTitle: 'Software Engineer',
+    jobTitle: 'Software Engineer and Technical Architect',
     title: 'Welcome',
     subtitle: "I'm Lucas Vera, a",
     p1: "I possess a strong enthusiasm for creating digital and technology-driven products while continuously expanding my knowledge. With a particular affinity for cloud and serverless technologies, I am constantly seeking innovative avenues to refine my expertise",
     p2a: "I'm currently working as a",
     p2b: "at",
-    p2c: ", where I contribute with my expertise in software development, cloud and serverless technologies.",
+    p2c: ", where I contribute with my expertise in software development, cloud and serverless architectures.",
     p3a: "I live in the beautiful",
     p3b: "the city of eternal spring. I love playing videogames, watching movies and enjoy playing tennis and chess. I also like cats and dogs, and silly stuff like",
     p4: "Here are a few links in case you want to know more about my work or contact me:"
   },
   es: {
-    jobTitle: "Ingeniero de Software",
+    jobTitle: 'Ingeniero de Software y Arquitecto Técnico',
     title: "Bienvenido",
     subtitle: "Soy Lucas Vera, un",
     p1: "Poseo un fuerte entusiasmo por crear productos digitales y tecnológicos mientras continuamente expando mis conocimientos. Tengo afinidad por las tecnologías en la nube y 'serverless', y estoy constantemente buscando vías innovadoras para mejorar mi experiencia",
     p2a: "Actualmente trabajo como",
     p2b: "en",
-    p2c: ", donde contribuyo con mi experiencia en desarrollo de software, tecnologías en la nube y 'serverless'.",
+    p2c: ", donde contribuyo con mi experiencia en desarrollo de software, arquitecturas en la nube y 'serverless'.",
     p3a: "Vivo en la hermosa",
     p3b: "la ciudad de la eterna primavera. Me encanta jugar videojuegos, ver películas y disfrutar del tenis y ajedrez. También me gustan los gatos y los perros, y cosas tontas como",
     p4: "Aquí hay algunos enlaces en caso de que quieras saber más sobre mi trabajo o desees contactarme:",
